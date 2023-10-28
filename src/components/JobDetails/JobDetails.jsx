853e30122db94c705c1637a461e48af9e9581225
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-10-28 21:24:32 +0600

    local storage added

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,10 +1,16 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const JobDetails = () => {
     const jobs = useLoaderData();
     const { id } = useParams();
     const idInt = parseInt(id)
     const job = jobs.find(job => job.id === idInt)
+
+    const handleApplyJob = () => {
+        toast('You have applied Successfully')
+    }
     return (
         <div>
             <div className="grid gap-4 md:grid-cols-4">
@@ -15,9 +21,10 @@ const JobDetails = () => {
                 </div>
                 <div className="border">
                     <h2 className="text-2xl">Side things</h2>
-                    <button className="btn btn-primary w-full">Apply Now</button>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
