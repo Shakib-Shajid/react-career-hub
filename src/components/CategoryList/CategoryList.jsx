c451fd5b7a082fc02c16c5a14c8db35de1869984
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-10-28 15:27:10 +0600

    card added

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -2,7 +2,8 @@
 const CategoryList = () => {
     return (
         <div>
-
+            <h2 className="text-5xl text-center">Job Category List</h2>
+            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, rerum? Impedit alias quis et atque numquam reprehenderit, sequi nobis qui.</p>
         </div>
     );
 };
--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 
 const FeaturedJobs = () => {
+
+    const [jobs, setJobs] = useState([]);   //For put data
+
+    useEffect(() => {
+        fetch('jobs.json')
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, [])             //for load data
     return (
         <div>
-
+            <div className="text-center">
+                <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat id distinctio ipsam aperiam placeat incidunt quasi voluptatum ullam ut omnis?</p>
+            </div>
+            <div className="grid grid-cols-2 gap-6">
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
         </div>
     );
 };
